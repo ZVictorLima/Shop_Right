15e2cd10a63f10fcba6db2cd07e98c8c6b81bd34
VincentRet <115279323+VincentRet@users.noreply.github.com>
2024-11-13 14:56:05 -0500

    single test edit

--- PlanogramApp/components/PlanogramSuccess.js
+++ PlanogramApp/components/PlanogramSuccess.js
@@ -100,7 +100,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         fontWeight: 'bold',
-        marginBottom: 20,
+        marginVertical: 20,
         textAlign: 'center',
     },
     infoContainer: {
